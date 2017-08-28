47c5d9a5b94efc2c517dcf377dd8c1fc5598bc8b
mantasBrusokas <mantasbrusokas@gmail.com>
2017-08-28 20:58:35 +0300

    Upadte

--- Vartotojai/vartotojaiController.js
+++ Vartotojai/vartotojaiController.js
@@ -1,4 +1,4 @@
-app.controller('meniuController', function ($scope, $http) {
+app.controller('vartotojaiController', function ($scope, $http) {
     console.log('Meniu kontroleris veikia');
 
     $http.get('Back-end/select-data.php').then(function (res) {
