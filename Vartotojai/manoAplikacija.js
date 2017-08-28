9e76c1e64d439e83d8cfb6819b1e19346d733f4d
mantasBrusokas <mantasbrusokas@gmail.com>
2017-08-28 21:15:54 +0300

    Upadte

--- Vartotojai/manoAplikacija.js
+++ Vartotojai/manoAplikacija.js
@@ -1,1 +1,1 @@
-var app = angular.module('manoAplikacija', ["ngRoute"]);
+var app = angular.module('manoAplikacija', []);
--- Vartotojai/vartotojaiController.js
+++ Vartotojai/vartotojaiController.js
@@ -1,7 +1,7 @@
 app.controller('vartotojaiController', function ($scope, $http) {
     console.log('Meniu kontroleris veikia');
 
-    $http.get('Back-end/select-data.php').then(function (res) {
+    $http.get('/Back-end/select-data.php').then(function (res) {
         $scope.users = res.data;
 
     })
